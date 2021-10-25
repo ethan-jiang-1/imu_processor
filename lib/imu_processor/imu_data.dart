49e92a140abcac603be5c2ffca98ed74ffa309d9
EthanMac <EthanMac@gmail.com>
2021-10-25 13:06:29 +0800

    remove average gravity in data

--- lib/imu_processor/imu_data.dart
+++ lib/imu_processor/imu_data.dart
@@ -63,11 +63,41 @@ class ImuData {
     return true;
   }
 
+  List getAverageAcce() {
+    double atx = 0.0;
+    double aty = 0.0;
+    double atz = 0.0;
+    for (int ndx = 0; ndx < _mLstImu.length; ndx += 1) {
+      List<double> imu = _mLstImu[ndx];
+      atx += imu[0];
+      aty += imu[1];
+      atz += imu[2];
+    }
+    atx /= _mLstImu.length;
+    aty /= _mLstImu.length;
+    atz /= _mLstImu.length;
+    return [atx, aty, atz];
+  }
+
+  bool removeAverageGravity() {
+    List atl = getAverageAcce();
+    double atx = atl[0];
+    double aty = atl[1];
+    double atz = atl[2];
+    for (int ndx = 0; ndx < _mLstImu.length; ndx += 1) {
+      List<double> imu = _mLstImu[ndx];
+      imu[0] -= atx;
+      imu[1] -= aty;
+      imu[2] -= atz;
+    }
+    return true;
+  }
+
   bool removeGravity() {
     if (_mGravityRemoved) {
       return true;
     }
-
+    removeAverageGravity();
     _mGravityRemoved = true;
     return true;
   }
--- test/imu_processor/imu_data_grv_test.dart
+++ test/imu_processor/imu_data_grv_test.dart
@@ -1,9 +1,53 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:imu_processor/imu_processor/imu_data.dart';
 
+void feed_data_var(ImuData data, int num_points, int var_mode) {
+  for (int i = 0; i < num_points; i++) {
+    int ts = (i / 10).toInt();
+    int hn = (i / 100).toInt();
+    int sign = 1;
+    if (i % var_mode == 1) {
+      sign = -1;
+    }
+    if (hn % 2 == 0) {
+      data.feed(
+          ts, sign * 1.0, sign * 1.0, sign * 1.0, 1.0 + i, 1.0 + i, 1.0 + i);
+    } else {
+      data.feed(
+          ts, sign * 0.5, sign * 0.5, sign * 0.5, 1.0 + i, 1.0 + i, 1.0 + i);
+    }
+  }
+}
+
 void main() {
-  testWidgets('imu data gravity filter...', (tester) async {
-    //ImuData idat = ImuData(50);
- 
+  testWidgets('imu data gravity filter0...', (tester) async {
+    ImuData idat = ImuData(50);
+
+    feed_data_var(idat, 100, 10);
+
+    List lstAcce0 = idat.getAverageAcce();
+    print(lstAcce0);
+    idat.removeGravity();
+    List lstAcce1 = idat.getAverageAcce();
+    print(lstAcce1);
+
+    expect((lstAcce1[0] * 10).toInt(), 0);
+    expect((lstAcce1[0] * 10).toInt(), 0);
+    expect((lstAcce1[0] * 10).toInt(), 0);
+  });
+  testWidgets('imu data gravity filter1...', (tester) async {
+    ImuData idat = ImuData(50);
+
+    feed_data_var(idat, 100, 1);
+
+    List lstAcce0 = idat.getAverageAcce();
+    print(lstAcce0);
+    idat.removeGravity();
+    List lstAcce1 = idat.getAverageAcce();
+    print(lstAcce1);
+
+    expect((lstAcce1[0] * 10).toInt(), 0);
+    expect((lstAcce1[0] * 10).toInt(), 0);
+    expect((lstAcce1[0] * 10).toInt(), 0);
   });
 }
